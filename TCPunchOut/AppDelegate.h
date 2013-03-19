ab9e1737488096f3a2f0add95dcb03f957f9983e
Theo Calmes <theocalmes@gmail.com>
2013-03-19 04:58:27 -0700

    First commit

---
+++ TCPunchOut/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  TCPunchOut
+//
+//  Created by Theodore Calmes on 3/19/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ TCPunchOut/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  TCPunchOut
+//
+//  Created by Theodore Calmes on 3/19/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
